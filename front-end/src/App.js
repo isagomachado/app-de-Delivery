4cdb68443d5fb730c66115621f6d0a8355016a05
Eric Santos <eric.silvasantos44@gmail.com>
2022-09-22 20:26:51 -0300

    Criação inicial da pagina de checkout
    Co-authored-by: Deyvid Cavalcante <deyvidscm@hotmail.com>

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,12 +5,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> } />
       <Route path="/login" element={ <Login /> } />
+      <Route path="/customer/checkout" element={ <Checkout /> } />
     </Routes>
   );
 }
