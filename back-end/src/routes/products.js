8b1360fb13b07bcfe5a82884f8972950fc7dd11d
Kaue Alves <kaue_alves00@yahoo.com>
2022-09-22 16:38:38 -0300

    fix/ corrigindo erros de lint

--- back-end/src/routes/products.js
+++ back-end/src/routes/products.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const ProductsController = require('../controllers/products')
+const ProductsController = require('../controllers/products');
 // const tokenMiddleware = require('../middlewares/tokenMiddleware')
+// tokenMiddleware
 
 const products = Router();
 
 products.route('/')
-  .get(/*tokenMiddleware,*/ ProductsController.getAll);
+  .get(ProductsController.getAll);
 
 module.exports = products;
