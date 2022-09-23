f79ce7795c9c12451a5b1ff7f48060b7589382c7
Kaue Alves <kaue_alves00@yahoo.com>
2022-09-22 23:21:57 -0300

    feat/ concluindo o requisito 13

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,24 +5,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
-<<<<<<< HEAD
 import Register from './pages/Register';
-import CostumerProducts from './pages/CostumerProducts';
-=======
 import Products from './pages/Products';
->>>>>>> main-group-14-fluxo-cliente-customer-products-2
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/customer/products" element={ <CostumerProducts /> } />
+      <Route exact path="/customer/products" element={ <Products /> } />
       <Route exact path="/register" element={ <Register /> } />
-      <Route path="/login" element={ <Login /> } />
-<<<<<<< HEAD
-      <Route path="/" element={ <Home /> } />
-=======
-      <Route path="/customer/products" element={ <Products /> } />
->>>>>>> main-group-14-fluxo-cliente-customer-products-2
+      <Route exact path="/login" element={ <Login /> } />
+      <Route exact path="/" element={ <Home /> } />
     </Routes>
   );
 }
