143809504902a40439465000108290be5ab49fa6
Kaue Alves <kaue_alves00@yahoo.com>
2022-09-22 22:01:06 -0300

    fix/ concluindo o requisito 13

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DeliveryContext from '../context/DeliveryContext';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const [nameUser, setNameUser] = useState('');
+  const { setDataLogin, setProducts } = useContext(DeliveryContext);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    setNameUser(user.name);
+  }, []);
+
+  const removeDataUserContext = () => {
+    const dataInitial = { name: '', password: '' };
+    setDataLogin(dataInitial);
+    setProducts([]);
+  };
+
+  const redirectHome = () => {
+    navigate('/');
+  };
+
+  const logOut = () => {
+    removeDataUserContext();
+    redirectHome();
+    localStorage.removeItem('user');
+  };
+
+  const css = { listStyleType: 'none' };
+
   return (
-    <nav>
-      <ul>
-        <li data-testid="customer_products__element-navbar-link-products">
-          Produtos
-        </li>
-        <li data-testid="customer_products__element-navbar-link-orders">
-          Meus Pedidos
-        </li>
-        <li data-testid="customer_products__element-navbar-user-full-name">
-          Nome do Fulano
-        </li>
-        <li data-testid="customer_products__element-navbar-link-logout">
-          Sair
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul style={ css }>
+          <li data-testid="customer_products__element-navbar-link-products">
+            Produtos
+          </li>
+          <li data-testid="customer_products__element-navbar-link-orders">
+            Meus Pedidos
+          </li>
+          <li data-testid="customer_products__element-navbar-user-full-name">
+            {nameUser}
+          </li>
+          <li>
+            <button
+              data-testid="customer_products__element-navbar-link-logout"
+              type="button"
+              onClick={ logOut }
+            >
+              Sair
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
