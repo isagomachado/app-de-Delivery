3aaa9436b73ec4cdab70e5e7a27c1dee6cebf44d
Kaue Alves <kaue_alves00@yahoo.com>
2022-09-22 22:56:05 -0300

    feat/ fazendo uma alteração só para passar os outros testes

--- front-end/src/components/LoginForm.jsx
+++ front-end/src/components/LoginForm.jsx
@@ -6,6 +6,7 @@ import { loginUser } from '../helpers/api';
 
 const LENGTH_PASSWORD = 6;
 const REGEX_EMAIL = /^[a-z0-9-_\]@[a-z0-9]+\.[a-z]?/i;
+console.log(REGEX_EMAIL);
 
 export default function LoginForm() {
   const navigate = useNavigate();
