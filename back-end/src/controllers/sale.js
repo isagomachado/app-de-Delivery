12d2f1d37560b4a685e80df955c443daf545b075
Eric Santos <eric.silvasantos44@gmail.com>
2022-09-27 16:25:02 -0300

    correção de lint

--- back-end/src/controllers/sale.js
+++ back-end/src/controllers/sale.js
@@ -5,12 +5,12 @@ const SaleService = require('../services/sale');
 
 class SaleController {
   static async create(req, res) {
-    const { objSale, products } = req.body;
+    const { objSale } = req.body;
     // console.log(req.header);
     // await Validate.saleBody(req.body);
     const user = LoginService.validateToken(req.headers.authorization);
     // objSale.userId = user.payload.id;
-    const sale = await SaleService.create({ ...objSale, userId: user.payload.id});
+    const sale = await SaleService.create({ ...objSale, userId: user.payload.id });
     // await SaleProductsService.create(products);
     return res.status(201).json({ id: sale.id, header: req.headers, user });
   }
